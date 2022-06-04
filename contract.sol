f4b4406a3c1f33eac75be2fe985c5d01af3fc5d5
jagadhis <jwaran78@gmail.com>
2022-06-04 10:10:47 +0530

    added Comments

--- contract.sol
+++ contract.sol
@@ -1,21 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract ZombieWorld{
-    uint dnaDigits = 16;
-    uint dnaModulus = 10 ** dnaDigits;
+contract ZombieWorld{   //creating a Contract 
+    uint dnaDigits = 16;   //initiating a unsigned integer 
+    uint dnaModulus = 10 ** dnaDigits; //using a Math Operator - Exponential Operator
 
-    struct Zombie {
-        string name;
+    struct Zombie {   //creating a Struct 
+        string name;   //adding variables with data type
     uint dna;
     
 }
-  Zombie[] public zombies;
+  Zombie[] public zombies;  //Creating Struct array With Public 
 
-  function createZombie(string memory _name,uint _dna) public{
-      zombies.push(Zombie(_name,_dna));
+  function createZombie(string memory _name,uint _dna) public{   //creating Funciton With Public Visibility and adding function parameters
+      zombies.push(Zombie(_name,_dna)); //pushing the paramteres value in the Struct array
   }
-  function _createZombie(string memory _name,uint _dna) private{
+  function _createZombie(string memory _name,uint _dna) private{  //creating Funciton with Private Visibility and adding function parameters
       zombies.push(Zombie(_name,_dna));
   }
 
