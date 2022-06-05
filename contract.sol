f0ec608d65259aa54ab7b93123319494f2fe647e
jagadhis <jwaran78@gmail.com>
2022-06-05 09:57:35 +0530

    Sticking with Error

--- contract.sol
+++ contract.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.7;
 
 contract ZombieWorld{   //creating a Contract 
+
+    event NewZombie(uint zombieId,string name,uint dna);
     uint dnaDigits = 16;   //initiating a unsigned integer 
     uint dnaModulus = 10 ** dnaDigits; //using a Math Operator - Exponential Operator
 
@@ -14,11 +16,14 @@ contract ZombieWorld{   //creating a Contract
 
   function createZombie(string memory _name,uint _dna) public{   //creating Funciton With Public Visibility and adding function parameters
       zombies.push(Zombie(_name,_dna)); //pushing the paramteres value in the Struct array
+     
   }
-  function _createZombie(string memory _name,uint _dna) private{  //creating Funciton with Private Visibility and adding function parameters
-      zombies.push(Zombie(_name,_dna));
-  }
-
+    function _createZombie(string memory _name, uint _dna) private {
+        zombies.push(Zombie(_name, _dna));
+      
+          uint id = zombies.push(Zombie(_name, _dna)) ;
+        emit NewZombie(id, _name, _dna);
+    }
   function _generateRandomDna(string memory _str) private view returns (uint){  //view function only viewing the data but it cannot be modified 
   keccak256(abi.encodePacked(_str));   //hashfunction keccak256 inbuilt version of SHA3
             uint rand = uint(keccak256(abi.encodePacked(_str)));   //typecasting
