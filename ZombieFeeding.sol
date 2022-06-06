31e9fae21d155cfe3d99509df271d1a0b630466e
jagadhis <jwaran78@gmail.com>
2022-06-06 11:57:34 +0530

    added comments

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -7,10 +7,10 @@ import "./ZombieWorld.sol";
 
 contract ZombieFeeding is ZombieWorld {  
     function feedAndMultiply(uint _zombieId,uint _targetDna) public{
-        require(msg.sender == zombieToOwner[_zombieId]);
-    Zombie storage myZombie = zombies[_zombieId];
-     _targetDna = _targetDna % dnaModulus;
-     uint newDna = (myZombie.dna + _targetDna) / 2;
-    _createZombie("NoName", newDna);
+        require(msg.sender == zombieToOwner[_zombieId]);  //checking that sender is same to ID of Zombie
+    Zombie storage myZombie = zombies[_zombieId];  //storing the zombieId in an array
+     _targetDna = _targetDna % dnaModulus;  //checking the data is in 16 bits long
+     uint newDna = (myZombie.dna + _targetDna) / 2;    //calaculating a New zombie DNA using average method
+    _createZombie("NoName", newDna);  //creating a zombie with a nname form create zombie function call
     }
 }
